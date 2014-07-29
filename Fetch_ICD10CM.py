5bc33271742eaaefaacaf7385457b6c8328cfbca
renchin <renchin.wu@gmail.com>
2014-07-29 18:04:07 -0400

    Update Fetch_ICD10CM.py

--- Fetch_ICD10CM.py
+++ Fetch_ICD10CM.py
@@ -1,10 +1,11 @@
 # Original script from http://stackoverflow.com/questions/3898574/google-search-using-python-script
 
 import urllib2
+import urllib.urlencode
 import json
 query = raw_input ( 'Query: ' )
 query = urllib.urlencode ( { 'q' : query } )
-response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query + '+site%3Awww.icd10data.com').read()
+response = urllib.urlopen ( 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query + ' site:www.icd10data.com').read()
 list1 = json.loads(response)
 
 #print list1
